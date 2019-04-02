5708c2dc2e665f96d507ca7ef1374e2d40334401
amannmaa2014 <amannmaa2014@gmail.com>
2019-04-02 13:56:50 +0300

    product-views

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import (
     UserCreateAPIView,
     CategorysList,
+    ProductDetailAPIView,
+    ProductDetailAPIView,
 )
 from rest_framework_jwt.views import obtain_jwt_token
 
