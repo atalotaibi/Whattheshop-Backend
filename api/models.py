ea4aa4e9783886c8116adcc7e72bdcbdb1db5d33
atalotaibi <alotaibiatallah@gmail.com>
2019-04-03 11:58:42 +0300

    url editing

--- api/models.py
+++ api/models.py
@@ -5,10 +5,10 @@ from django.db.models.signals import post_save
 
 
 class Category(models.Model):
-	name = models.CharField(max_length=120)
+    name = models.CharField(max_length=120)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 # profile
@@ -17,47 +17,47 @@ class Category(models.Model):
 
 
 class Profile(models.Model):
-	user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
-	dob = models.DateField(null=True)
-	GENDERS = (
-		('M', 'Male'),
-		('F', 'Female'),
-	)
-	gender = models.CharField(
-		max_length=2,
-		choices=GENDERS,
-		default='F',
-	)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    dob = models.DateField(null=True)
+    GENDERS = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(
+        max_length=2,
+        choices=GENDERS,
+        default='F',
+    )
 
-	def __str__(self):
-		return self.user.username
+    def __str__(self):
+        return self.user.username
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwarg):
-	if created:
-		Profile.objects.create(user=instance)
+    if created:
+        Profile.objects.create(user=instance)
 
 
 # address
 # forign key of the profile and has a related name
 # address detail related
 class Address(models.Model):
-	profile = models.ForeignKey(
-		Profile, related_name='addresses', default=1, on_delete=models.CASCADE)
-	description = models.TextField()
+    profile = models.ForeignKey(
+        Profile, related_name='addresses', default=1, on_delete=models.CASCADE)
+    description = models.TextField()
 
 
 class Product(models.Model):
-	name = models.CharField(max_length=120)
-	description = models.TextField()
-	stock = models.IntegerField(default=0)
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    stock = models.IntegerField(default=0)
 
-	category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
-	price = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=3, default=0)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
     # def image(self):
     #     return self.images.first()
@@ -70,14 +70,14 @@ class Product(models.Model):
 
 
 class Order(models.Model):
-	profile = models.ForeignKey(
-		Profile, related_name='orders', default=1, on_delete=models.CASCADE)
-	date = models.DateField()
-	time = models.TimeField()
+    profile = models.ForeignKey(
+        Profile, related_name='orders', default=1, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
 
-	def total_price(self):
-		
-		 return sum(self.cartItems.all().values_list('sub_total', flat=True))
+    def total_price(self):
+
+        return sum(self.cartItems.all().values_list('sub_total', flat=True))
 
 
 # takes the profile as a foreign key
@@ -86,26 +86,26 @@ class Order(models.Model):
 # takes quantity as an iteger
 # total price is an integer and is the total of quantity * the product price
 class CartItem(models.Model):
-	# profile = models.ForeignKey(Profile, default=1, on_delete=models.CASCADE)
-	product = models.ForeignKey(Product, default=1, on_delete=models.CASCADE)
-	order = models.ForeignKey(
-		Order, related_name='cartItems', default=1, on_delete=models.CASCADE)
-	quantity = models.IntegerField(default=0)
-	sub_total = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    # profile = models.ForeignKey(Profile, default=1, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, default=1, on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order, related_name='cartItems', default=1, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    sub_total = models.DecimalField(max_digits=10, decimal_places=3, default=0)
 
-	def get_sub_total(self):
-		self.sub_total = self.quantity * self.product.price
+    def get_sub_total(self):
+        self.sub_total = self.quantity * self.product.price
 
 # ----------***added an image field***------------
 
+
 class Image(models.Model):
 
-	product = models.ForeignKey(
-		Product, related_name='images', default=1, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, related_name='images', default=1, on_delete=models.CASCADE)
 
-    image = models.ImageField(upload_to='products_photos', null=True, blank=True)
-    
+    image = models.ImageField(
+        upload_to='products_photos', null=True, blank=True)
+
     def __str__(self):
         return self.product
-
-
--- api/urls.py
+++ api/urls.py
@@ -2,13 +2,10 @@ from django.urls import path
 from .views import (
     UserCreateAPIView,
     CategorysList,
-
     OrderListView,
-    OrderDetailView
-
+    OrderDetailView,
     ProductListAPIView,
     ProductDetailAPIView,
-
 )
 from rest_framework_jwt.views import obtain_jwt_token
 
@@ -17,10 +14,12 @@ urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('CategorysList/', CategorysList.as_view(), name='CategorysList'),
 
-    path('api/order/list/', OrderListView.as_view(), name='api-order-list'),
-    path('api/order/<int:order_id>/detail/', OrderDetailView.as_view(), name='api-order-detail'),
+    path('order/list/', OrderListView.as_view(), name='api-order-list'),
+    path('order/<int:order_id>/detail/',
+         OrderDetailView.as_view(), name='api-order-detail'),
 
     path('product/list/', ProductListAPIView.as_view(), name='product-list'),
-    path('product/detail/<int: product_id>/', ProductDetailAPIView.as_view(), name='product-detail'),
+    path('product/detail/<int: product_id>/',
+         ProductDetailAPIView.as_view(), name='product-detail'),
 
 ]
